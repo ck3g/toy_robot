d337125c2db6088bcbb0aeed6a3c3d5bbf3891a9
Vitaly Tatarintsev <Kalastiuz@gmail.com>
2017-11-17 16:06:02 +0100

    Receive reports about position of the robot

--- lib/toy_robot.ex
+++ lib/toy_robot.ex
@@ -6,4 +6,8 @@ defmodule ToyRobot do
   def place(x, y, facing) do
     %ToyRobot.Position{x: x, y: y, facing: facing}
   end
+
+  def report(%ToyRobot.Position{x: x, y: y, facing: facing} = _robot) do
+    {x, y, facing}
+  end
 end
--- test/toy_robot_test.exs
+++ test/toy_robot_test.exs
@@ -9,4 +9,9 @@ defmodule ToyRobotTest do
   test "places the Toy Robot on the table in the specified position" do
     assert ToyRobot.place(1, 2, :south) == %ToyRobot.Position{x: 1, y: 2, facing: :south}
   end
+
+  test "provides the report of the robot's position" do
+    robot = ToyRobot.place(2, 3, :west)
+    assert ToyRobot.report(robot) == {2, 3, :west}
+  end
 end
