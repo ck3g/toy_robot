7842321c9a08f543d227565a3452750cf2909a50
Vitaly Tatarintsev <Kalastiuz@gmail.com>
2017-11-24 17:09:08 +0100

    Add ExDoc as a dependency

--- mix.exs
+++ mix.exs
@@ -21,6 +21,7 @@ defmodule ToyRobot.Mixfile do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:ex_doc, only: :dev, runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
