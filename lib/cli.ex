5ce37dc377dde41b354c7c3380a0de3c98b3445b
Vitaly Tatarintsev <Kalastiuz@gmail.com>
2017-12-08 15:06:37 +0100

    Start listening for commands and "quit" if needed

--- lib/cli.ex
+++ lib/cli.ex
@@ -1,5 +1,35 @@
 defmodule ToyRobot.CLI do
   def main(_args) do
     IO.puts("Welcome to the Toy Robot simulator!")
+    print_help_message()
+    receive_command()
+  end
+
+  @commands %{
+    "quit" => "Quits the simulator"
+  }
+
+  defp receive_command do
+    IO.gets("\n> ")
+    |> String.trim
+    |> String.downcase
+    |> execute_command
+  end
+
+  defp execute_command("quit") do
+    IO.puts "\nConnection lost"
+  end
+
+  defp execute_command(_unknown) do
+    IO.puts("\nInvalid command. I don't know what to do.")
+    print_help_message()
+
+    receive_command()
+  end
+
+  defp print_help_message do
+    IO.puts("\nThe simulator supports following commands:\n")
+    @commands
+    |> Enum.map(fn({command, description}) -> IO.puts("  #{command} - #{description}") end)
   end
 end
