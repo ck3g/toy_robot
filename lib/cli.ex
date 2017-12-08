32d5d4430295b41fcd6e673af18c7e2eb03d0463
Vitaly Tatarintsev <Kalastiuz@gmail.com>
2017-12-08 16:48:42 +0100

    [CLI] Implement place command

--- lib/cli.ex
+++ lib/cli.ex
@@ -6,17 +6,38 @@ defmodule ToyRobot.CLI do
   end
 
   @commands %{
-    "quit" => "Quits the simulator"
+    "quit" => "Quits the simulator",
+    "place" => "format: \"place [X,Y,F]\". " <>
+               "Places the Robot into X,Y facing F (Default is 0,0,North). " <>
+               "Where facing is: north, west, south or east."
   }
 
   defp receive_command do
-    IO.gets("\n> ")
+    IO.gets("> ")
     |> String.trim
     |> String.downcase
+    |> String.split(" ")
     |> execute_command
   end
 
-  defp execute_command("quit") do
+  defp execute_command(["place"]) do
+    ToyRobot.place
+    receive_command()
+  end
+
+  defp execute_command(["place" | params]) do
+    {x, y, facing} = process_place_params(params)
+
+    case ToyRobot.place(x, y, facing) do
+      {:ok, _robot} ->
+        receive_command()
+      {:failure, message} ->
+        IO.puts message
+        receive_command()
+    end
+  end
+
+  defp execute_command(["quit"]) do
     IO.puts "\nConnection lost"
   end
 
@@ -27,9 +48,15 @@ defmodule ToyRobot.CLI do
     receive_command()
   end
 
+  defp process_place_params(params) do
+    [x, y, facing] = params |> Enum.join("") |> String.split(",") |> Enum.map(&String.trim/1)
+    {String.to_integer(x), String.to_integer(y), String.to_atom(facing)}
+  end
+
   defp print_help_message do
     IO.puts("\nThe simulator supports following commands:\n")
     @commands
     |> Enum.map(fn({command, description}) -> IO.puts("  #{command} - #{description}") end)
+    IO.puts("")
   end
 end
